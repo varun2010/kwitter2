b766488059418cd8ff6400d15d80b9bac1f824a8
varun2010 <76247848+varun2010@users.noreply.github.com>
2021-02-04 14:58:15 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC5bbGKkbqsdWh12Iln7K4ophBF-BkKPEM",
+    authDomain: "kwitter-project-3e24e.firebaseapp.com",
+    databaseURL: "https://kwitter-project-3e24e-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-3e24e",
+    storageBucket: "kwitter-project-3e24e.appspot.com",
+    messagingSenderId: "674952562604",
+    appId: "1:674952562604:web:8de17b39cca6629b039c09",
+    measurementId: "G-2ERS3F2RV0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
